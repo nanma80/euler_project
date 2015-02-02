996508ccab3ce82e26d932e69cba5b88d2b5b5f5
Nan Ma <nma@yammer-inc.com>
2015-02-02 18:27:03 +0800

    216 v1

---
+++ p201_225/p216/v1.rb
@@ -0,0 +1,34 @@
+require '../../common'
+require 'benchmark'
+
+def main
+  limit = 5 * (10 ** 5)
+  # limit = 10 ** 3
+  prime_limit = (limit * 1.42).to_i
+  prime_cache = Prime::Cache.new(prime_limit)
+  primes = prime_cache.primes.select {|prime| prime % 8 == 1 || prime % 8 == 7}
+  puts "cache finished"
+
+  count = 0
+  (2..limit).each do |n|
+    puts 100 * n / limit if n % (10 ** 4) == 0
+    t = 2 * n * n - 1
+    sqrt_t = Math.sqrt(t).to_i
+    is_prime = true
+    primes.each do |prime|
+      break if prime > sqrt_t
+      if t % prime == 0
+        is_prime = false
+        break
+      end
+    end
+
+    if is_prime
+      count += 1
+    end
+  end
+
+  p count
+end
+
+p Benchmark.measure { main }
